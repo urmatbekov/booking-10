56a1a4e7f4af6935bd409fc3f5d0cbc8eb3553a2
Your Name <you@example.com>
2021-05-06 15:21:17 +0300

    template

--- mysite/client/views.py
+++ mysite/client/views.py
@@ -4,8 +4,5 @@ from client.models import Client
 # Create your views here.
 def client(request):
     clients = Client.objects.all()
-    results = "<ul>"
-    for cl in clients:
-        results += "<li>{} {} {}</li> ".format(cl.name,cl.surname,cl.age)
-    results += "</ul>"
-    return render(request,"client/client.html")
+    
+    return render(request,"client/client.html",{"clients":clients})
