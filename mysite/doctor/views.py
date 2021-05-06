f9b33a863129bdbe55849aab9d3fc2a18f04efe6
Your Name <you@example.com>
2021-05-06 16:25:10 +0300

    template

--- mysite/doctor/views.py
+++ mysite/doctor/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
 from doctor.models import Doctor
-from django.http import HttpResponse
 
 # Create your views here.
 def doctor(request):
     doctors = Doctor.objects.all()
-    result = ""
-    for doc in doctors:
-        result += "{} {}, ".format(doc.name,doc.surname)
-    return HttpResponse(result)
+    return render(request,"doctor/doctor.html",{"doctors":doctors})
